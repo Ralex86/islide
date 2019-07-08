7e3150379c0b8ae55daf8af1ce56a7c5f54941d9
Alex <alexandre@hassler.Fr>
2019-07-08 14:04:41 +0200

    production server

---
+++ dist/iremote/precache-manifest.f11c2580559ab2588c8d422e2990c403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e70d0b1670e0dc55b3095c2746e327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9354e887b520a1da7f2",
+    "url": "/static/css/main.f05b058f.chunk.css"
+  },
+  {
+    "revision": "0da94df4776e48ae0591",
+    "url": "/static/js/2.fdf826ff.chunk.js"
+  },
+  {
+    "revision": "c9354e887b520a1da7f2",
+    "url": "/static/js/main.31504092.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
